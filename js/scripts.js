e10cef4c2f228a1b7ec16f9c5d6512b2fe4ba7c0
Maria Del Castillo <maria.f.delcastillo@gmail.com>
2017-05-12 10:05:42 -0700

    Add initial JS for collecting user input

--- js/scripts.js
+++ js/scripts.js
@@ -7,4 +7,21 @@ function Pizza(size, toppings) {
 
 Pizza.prototype.getPrice = function()
     var price = 10;
-    
+//
+return price;
+
+//Front-End Logic:
+$(document).ready(function() {
+  $("#formOne").submit(function(event) {
+
+    var inputtedSize = $("input[name=size]:checked").val();
+    var inputtedToppings = $("input[name=toppings]:checked").val();
+
+
+    var newPizza = new Pizza(inputtedSize, inputtedToppings);
+
+    $("#output").text("Your total cost is: $" + newPizza.getPrice() + ".00");
+
+    event.preventDefault();
+  });
+});
